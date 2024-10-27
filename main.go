ab7c7f511a35dfc0f46c47ea90f9048be3036626
StackOverfloweds <bryangithub32@gmail.com>
2024-10-27 19:51:04 +0700

    update: add setup model for database live chat

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"go-live-chat/config"
+	"go-live-chat/database"
+	"log"
+)
+
 func main() {
-	
+
+	config.Connect()
+	defer config.DB.Close()
+
+	database.CreateTables()
+	log.Println("Database setup completed")
+
 }
